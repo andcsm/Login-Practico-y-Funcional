6340b980c2dc9cdc69658fbda18163a7d505e986
AndCsm <blog.corposucre@gmail.com>
2019-04-08 09:47:06 -0500

    Se divide el archivo, se quita lo que solo tiene que ver con el formulario de login, se dejan las funciones comunes como show y hide response

--- app.js
+++ app.js
@@ -1,32 +1,4 @@
 
-// Obtenemos el formulario y el div con sus respectivos ID´s
-var form = document.getElementById('login-form');
-var response = document.getElementById('response');
-
-// Monitoremos los eventos del formulario para saber cuendo hacen un submit
-form.addEventListener('submit', function(event){
-    
-    //Se detinen las acciones por defecto del formulario al hacer submit
-    event.preventDefault();
-
-    //Obtenemos los datos ingresados en los campos del formulario
-    var userData = new FormData(form);
-
-    //Usamos el metodo Fetch API para enviar al servidor los datos de usuario a verifivar
-    fetch('verify-user.php',{
-        method: 'POST',
-        body: userData
-    })
-        .then( res => res.json())
-        .then( data => {
-            if(data === 'logedin'){
-                showResponse(0, 'Logeado con exito');
-            }else{
-                showResponse(1, data);
-            }
-        });
-});
-
 //Función para desplegar los mensages de error o exito usando el div con clase "response"
 function showResponse(goodOrBad, msg) {
     if (goodOrBad == 0) {
